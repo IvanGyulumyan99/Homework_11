bc70e149ff13feac9e0b5a55c6492159650edeea
IvanGyulumyan99 <ivan.gyulumyan@gmail.com>
2019-12-13 01:13:22 +0400

    2

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        //1
         int firstArray[] = {1,2,3,4,5,6,7,8};
         for(int elem:firstArray){
             System.out.print(elem + " ");
         }
         System.out.println();
+        //2
         int secondArray[] = new int[10];
-        for(int elem:secondArray){
-            System.out.print(elem + " ");
-        }
+
+
     }
 }
