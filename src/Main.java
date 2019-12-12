367b81ecf3157098f603838591e9fb19ff6e70ac
IvanGyulumyan99 <ivan.gyulumyan@gmail.com>
2019-12-13 01:43:19 +0400

    5

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         //1
-        int firstArray[] = {1,2,3,4,5,6,7,8};
-        for(int elem:firstArray){
+        int firstArray[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        for (int elem : firstArray) {
             System.out.print(elem + " ");
         }
         System.out.println();
@@ -10,19 +10,25 @@ public class Main {
         int secondArray[] = new int[10];
         //3
         int thirdArray[] = new int[1000];
-        for(int i = 1; i <= 1000; i++){
-            thirdArray[i-1] = i;
+        for (int i = 1; i <= 1000; i++) {
+            thirdArray[i - 1] = i;
         }
         //4
         byte fourthArray[] = new byte[30];
-        int counterForFourthArray = 0;
-        for(byte i = -30; i<31; i+=2){
-            if(i!=0) {
-                fourthArray[counterForFourthArray] = i;
-                counterForFourthArray++;
+        int indexForFourthArray = 0;
+        for (byte i = -30; i < 31; i += 2) {
+            if (i != 0) {
+                fourthArray[indexForFourthArray] = i;
+                indexForFourthArray++;
             }
         }
-
-
+        //5
+        short fifthArray[] = new short[150];
+        int indexForFifthArray = 0;
+        for (short i = 1; i < 301; i += 2) {
+            fifthArray[indexForFifthArray] = i;
+            indexForFifthArray++;
+        }
+        //6
     }
 }
